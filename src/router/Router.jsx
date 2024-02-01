7fa973ff08e1ea308f9575962790cf16d59923cb
Amir <106912785+am1r0d@users.noreply.github.com>
2024-02-01 19:40:31 +0330

    set accessToken in header with interceptors

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -12,6 +12,7 @@ function Router() {
     const { data, isLoading, error } = useQuery(["profile"], getProfile);
     console.log({ data, isLoading, error });
 
+    if (isLoading) return <h1>Loading...</h1>;
     return (
         <Routes>
             <Route index element={<HomePage />} />
--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,5 @@
 import api from "configs/api";
-import { getCookie } from "src/utils/cookie";
 
-const token = getCookie("accessToken");
-
-const getProfile = () =>
-    api.get("user/whoami", {
-        headers: { Authorization: `Bearer ${token} ` },
-    });
+const getProfile = () => api.get("user/whoami");
 
 export { getProfile };
