b5c452c1522ffeca17eb910146024d6b83e832c9
Amir <106912785+am1r0d@users.noreply.github.com>
2024-02-01 23:59:28 +0330

    update inputs && send category data to backend

--- src/components/template/CategoryForm.jsx
+++ src/components/template/CategoryForm.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { addCategory } from "services/admin";
 
 // styles
 import styles from "./CategoryForm.module.css";
 
 function CategoryForm() {
-    const [form, setForm] = useState({ name: "", slug: "", icon: " " });
+    const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+
+    const { mutate, isLoading, error, data } = useMutation(addCategory);
+    console.log({ isLoading, error, data });
+
     const changeHandler = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
     const submitHandler = (e) => {
         e.preventDefault();
-        console.log(form);
+        if (!form.name || !form.slug || !form.icon) return;
+        mutate(form);
     };
 
     return (
@@ -20,13 +27,19 @@ function CategoryForm() {
             className={styles.form}
         >
             <h3>دسته بندی جدید</h3>
+            {/* {!!error && <p>{error.message}</p>} */}
+            {!!error && <p>مشکلی پیش آمده</p>}
+            {data?.status === 201 && <p>دسته بندی با موفقیت اضافه شد.</p>}
+
             <label htmlFor="name">اسم دسته بندی</label>
             <input type="text" name="name" id="name" />
             <label htmlFor="slug">اسلاگ</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="slug" id="slug" />
             <label htmlFor="icon">آیکون</label>
-            <input type="text" name="name" id="name" />
-            <button type="submit">ایجاد</button>
+            <input type="text" name="icon" id="icon" />
+            <button type="submit" disabled={isLoading}>
+                ایجاد
+            </button>
         </form>
     );
 }
---
+++ src/services/admin.js
@@ -0,0 +1,4 @@
+import api from "configs/api";
+const addCategory = (data) => api.post("category", data);
+
+export { addCategory };
